54cbad89c62b2159355883308af083a89ddbe2e6
breverdbidder <brevardbidderai@gmail.com>
2026-01-21 12:07:36 -0500

    feat: Add components index

---
+++ v3/frontend/components/index.ts
@@ -0,0 +1,5 @@
+export { MapPanel } from './MapPanel';
+export { ChatPanel } from './ChatPanel';
+export { ChoroplethLayer, ZoningLegend } from './ChoroplethLayer';
+export { ParcelPopup } from './ParcelPopup';
+export { SplitScreen } from './SplitScreen';
